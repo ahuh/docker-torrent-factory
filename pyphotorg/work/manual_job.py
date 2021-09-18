739fe34ab50b1cded3c2ff3f67a1ed24e4ddd77d
ahuh <ahuh@free.fr>
2021-09-18 13:55:55 +0200

    FIX: manual job execution

--- pyphotorg/work/manual_job.py
+++ pyphotorg/work/manual_job.py
@@ -2,10 +2,14 @@ import job
 
 # ======================================
 # Main
-def main(argv):
+def main():
     print("=====================================")
     print("Manual job starting ...")
 
     job.job()
 
     print("... Manual job ended !")
+    
+if __name__ == "__main__":
+    # execute only if run as a script
+    main()
