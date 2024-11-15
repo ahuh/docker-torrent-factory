07fce5bf3ce43b5155d4c81fdb03f0bb2927bd13
ahuh <ahuh@outlook.fr>
2024-11-15 21:38:02 +0100

    Add Jackett container + selection of containers to enable in launcher DTF

--- launch-dtf.sh
+++ launch-dtf.sh
@@ -7,7 +7,10 @@
 # Set long timeout to prevent errors with 'docker compose up' command (slow on MyCloud EX2 Ultra)
 export COMPOSE_HTTP_TIMEOUT=600
 
+# Select containers to activate
+export DTF_CONTAINERS="transmission-openvpn joal medusa jackett kodi-mariadb nginx minidlna pyphotorg"
+
 # - Pull new images from docker hub (update)
 # - Start containers in detached mode
-docker compose pull && \
-docker compose up -d
+docker compose pull ${DTF_CONTAINERS} && \
+docker compose up -d ${DTF_CONTAINERS}
