2433db4bfd559c57d640f1403ecd11ff56609deb
ahuh <ahuh@outlook.fr>
2025-01-24 08:59:53 +0100

    FIX name for mariadb-upgrade command

--- kodi-upgrade-db.sh
+++ kodi-upgrade-db.sh
@@ -6,4 +6,4 @@
 
 # - Fix too permissive permissions on MariaDB config file
 # - Upgrade database for the current MariaDB version
-(source .env; docker compose exec -T kodi-mariadb sh -c "chmod 664 /config/custom.cnf && mysql_upgrade -u root -p'$KODI_DB_ROOT_PASSWORD'")
+(source .env; docker compose exec -T kodi-mariadb sh -c "chmod 664 /config/custom.cnf && mariadb-upgrade -u root -p'$KODI_DB_ROOT_PASSWORD'")
